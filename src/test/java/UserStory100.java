16dea5d6dc0a950f95cdbbb258c834de50f27c46
kgasimov <Kgasimov@gmail.com>
2021-06-30 19:47:57 -0400

    Fifth commit US100, made some changes to Second commit

--- src/test/java/UserStory100.java
+++ src/test/java/UserStory100.java
@@ -1,6 +1,6 @@
 public class UserStory100 {
     // Some code coming
-    // Writing new codes and commit
+    // Writing new codes and commit 1
     // Writing new codes and commit 2
     // Writing new codes and commit 3
 }
