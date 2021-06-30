1a93f42bcd806b155c5c9952021afc873087e3a2
kgasimov <Kgasimov@gmail.com>
2021-06-30 19:35:08 -0400

    Working on recap 3

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Recap like day 1");
+        System.out.println("One more commit");
+        System.out.println("Just work around");
     }
 }
